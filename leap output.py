a16d090feba3ba09a5beaee9ee59f007e2f321c4
Srinija1379 <112627711+Srinija1379@users.noreply.github.com>
2022-10-07 14:11:17 +0530

    Add files via upload

---
+++ leap output.py
@@ -0,0 +1,23 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\leap year or not.py 
+enter year:1947
+it is Non leap year 1947
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\leap year or not.py 
+enter year:1936
+it is a leap year 1936
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\leap year or not.py 
+enter year:0
+it is Non leap year 0
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\leap year or not.py 
+enter year:1428
+it is a leap year 1428
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\leap year or not.py 
+enter year:020
+it is a leap year 20
+>>> 
